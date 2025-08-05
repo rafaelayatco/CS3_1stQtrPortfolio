25150061711be1cedbdfbe293b3a9a6720cb5796
rafaelayatco <b2029rmyatco@pshs.edu.ph>
2025-08-05 09:01:31 +0800

    first non-graded exercise

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
 // no script tag here 
+
+
+let age = 14;
+let maxAge = 100; 
+let numPerDay = 5;
+
+totalRequired = (numPerDay * 365) * (maxAge - age);
+
+document.write("You will need " + totalRequired + " snacks to last you until the ripe old age of " + maxAge)
