1c7985c4c95296cb9900bb1708d6bf40b7cc5cea
rafaelayatco <b2029rmyatco@pshs.edu.ph>
2025-08-15 12:05:20 +0800

    non graded practice aug 15

--- public/script.js
+++ public/script.js
@@ -2,12 +2,13 @@
 // let age = window.prompt("Enter your age  ", "14");
 // let maxAge = window.prompt("Enter your maxAge  ", "100");
 // let  numPerDay = window.prompt("Enter your numPerDay  ", "5");
+// src = "script.js"
 
 if (confirm(`Is the ${age},${maxAge},${numPerDay} really correct?`)) {
 
-totalRequired = (numPerDay * 365) * (maxAge - age);
+let totalRequired = (Number(perDay * 365)) * (Number(maxAGE - age));
 
-console.log("You will need " + totalRequired + " snacks to last you until the ripe old age of " + maxAge);
+console.log("You will need " + totalRequired + " snacks to last you until the ripe old age of " + maxAGE);
 }
 
 else{
